38332597840b144bc3f184cb8fdc3eacb3402a9d
zz-giraffe <150102664+zz-giraffe@users.noreply.github.com>
2023-11-08 12:36:09 +0800

    add the contact button function

--- script.js
+++ script.js
@@ -1,1 +1,11 @@
-alert ("Hello")
+
+let contactButton = document.querySelector("button");
+
+function contactMe(){
+    let name=prompt("What is your name?");
+    let email=prompt("What is your email address?")
+    let h2=document.querySelector("h2");
+    h2.innerHTML=`Hello ${name}, we will send a welcome email to your address: ${email}!`
+}
+
+contactButton.addEventListener("click", contactMe);
